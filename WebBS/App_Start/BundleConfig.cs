13b79d0317358f4e553f9456f93ba07629f143e6
samuel1804 <https://github.com/samuel1804/boticasysalud.git>
2016-01-19 19:54:03 -0500

    inicio

---
+++ WebBS/App_Start/BundleConfig.cs
@@ -0,0 +1,18 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace WebBS.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {            
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/css/bootstrap.min.css",
+                      "~/Content/css/Site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.min.js"));
+        }
+    }
+}
---
+++ WebBS/Content/js/app.js
@@ -0,0 +1,16 @@
+﻿$(function () {
+    //Icon sidebar left
+    $('[data-toggle=offcanvas]').click(function () {
+        $(this).toggleClass('visible-xs text-center');
+        $(this).find('i').toggleClass('glyphicon-chevron-right glyphicon-chevron-left');
+        $('.row-offcanvas').toggleClass('active');
+        $('#lg-menu').toggleClass('hidden-xs').toggleClass('visible-xs');
+        $('#xs-menu').toggleClass('visible-xs').toggleClass('hidden-xs');
+        $('#btnShow').toggle();
+    });
+
+    //Fix scroll modal bootstrap
+    $(document).on('hidden.bs.modal', '.modal', function () {
+        $('.modal:visible').length && $(document.body).addClass('modal-open');
+    });
+});
