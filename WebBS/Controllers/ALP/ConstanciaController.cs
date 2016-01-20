fb5fa1662959c12ff69f5ff3f06203b87d17b1e0
Kenji Oki <Kenoki@live.com>
2016-01-20 03:50:41 -0500

    ConstanciaController - Se modificó el ActionResult Index para que devolviera las ordenes de preparado. - Se agregó el ActionResult Nuevo para poder registrar una nueva constancia.
    Vistas
    - Se modificó la vista de constancia para que liste las ordenes.
    - Se agregó la vista para el registro de constancia.

--- WebBS/Controllers/ALP/ConstanciaController.cs
+++ WebBS/Controllers/ALP/ConstanciaController.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebBS.Models;
 
 namespace WebBS.Controllers.ALP
 {
     public class ConstanciaController : Controller
     {
 
+        private BDBoticasEntities db = new BDBoticasEntities();
+
         public ActionResult Index()
         {
-            return View("~/Views/ALP/Constancia/Index.cshtml");
+
+            var ordenPreparado = db.ALP_ORDEN_PREPARADO;
+
+            return View("~/Views/ALP/Constancia/Index.cshtml", ordenPreparado);
+        }
+
+        public ActionResult Nuevo(string num_orden = null)
+        {
+            var ordenPreparado = db.ALP_ORDEN_PREPARADO.Where( o => o.num_orden_preparado.Contains(num_orden));
+            return View("~/Views/ALP/Constancia/Nuevo.cshtml", ordenPreparado);
         }
 
     }
