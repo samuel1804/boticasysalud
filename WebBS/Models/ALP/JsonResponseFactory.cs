ebc4d795ad94539811cdd454d137466782249647
Kenji Oki <Kenoki@live.com>
2016-01-21 02:13:46 -0500

    - Creación de clase de apoyo para devolver una respuesta JSON con el estado de la operación y un objeto adicional si deseamos. - Actualización de carpetas y elementos del archivo del proyecto. - Se actualizó la cadena de conexión.

---
+++ WebBS/Models/ALP/JsonResponseFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebBS.Models.ALP
+{
+    public class JsonResponseFactory
+    {
+
+        public static object ErrorResponse(string error)
+        {
+            return new { Success = false, ErrorMessage = error };
+        }
+
+        public static object SuccessResponse()
+        {
+            return new { Success = true };
+        }
+
+        public static object SuccessResponse(object referenceObject)
+        {
+            return new { Success = true, Object = referenceObject };
+        }
+
+    }
+}
