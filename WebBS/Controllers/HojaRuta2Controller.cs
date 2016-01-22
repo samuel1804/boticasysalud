465346d8ecf661c22d9c469255cd831192900d24
jledesma2509@gmail.com <analista25>
2016-01-21 22:10:41 -0500

    controlador hoja de ruta 2

---
+++ WebBS/Controllers/HojaRuta2Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebBS.Controllers
+{
+    public class HojaRuta2Controller : Controller
+    {
+        // GET: HojaRuta2
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
