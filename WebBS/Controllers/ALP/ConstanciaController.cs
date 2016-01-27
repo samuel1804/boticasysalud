380aeb55ed21a9144cbfe0b75f8610f802264b44
Kenji Oki <Kenoki@live.com>
2016-01-27 02:02:30 -0500

    Se modificó ConstanciaController - listar las constancias con filtro de búsqueda. - Modificar Constancia (Precarga y actualización)
    ControlMermaController
    - Listado de constancia

--- WebBS/Controllers/ALP/ConstanciaController.cs
+++ WebBS/Controllers/ALP/ConstanciaController.cs
@@ -38,6 +38,64 @@ namespace WebBS.Controllers.ALP
             return View("~/Views/ALP/Constancia/Index.cshtml");
         }
 
+        public ActionResult Lista()
+        {
+            List<SelectListItem> selectList = new List<SelectListItem>();
+
+            SelectListItem t = new SelectListItem();
+            t.Text = "Todos";
+            t.Value = "-1";
+            selectList.Add(t);
+
+            foreach (RRH_Sucursal c in db.RRH_Sucursal.ToList())
+            {
+                SelectListItem i = new SelectListItem();
+                i.Text = c.Nom_sucursal.ToString();
+                i.Value = c.Cod_sucursal.ToString();
+                selectList.Add(i);
+            }
+
+            ViewBag.Sucursales = selectList;
+
+            return View("~/Views/ALP/Constancia/Lista.cshtml");
+        }
+
+        [HttpPost]
+        public ActionResult BuscarConstancia(string nroConstancia, string fechaConstancia, string nomPreparado, int sucursal)
+        {
+
+            try
+            {
+
+                DateTime tmpFechaConstancia = DateTime.Parse(fechaConstancia);
+
+                var constancia = db.ALP_CONSTANCIA_PREPARADO.Where(o => o.estado == "01" &&
+                                                              o.num_constancia_preparado.Contains(String.IsNullOrEmpty(nroConstancia) ? o.num_constancia_preparado : nroConstancia) &&
+                                                              o.ALP_ORDEN_PREPARADO.ALP_RECETA.nom_preparado.Contains(String.IsNullOrEmpty(nomPreparado) ? o.ALP_ORDEN_PREPARADO.ALP_RECETA.nom_preparado : nomPreparado) &&
+                                                              o.ALP_ORDEN_PREPARADO.RRH_Sucursal.Cod_sucursal == (sucursal == -1 ? o.ALP_ORDEN_PREPARADO.RRH_Sucursal.Cod_sucursal : sucursal) &&
+                                                              o.fec_elaboracion.Year == (tmpFechaConstancia.Year > 0 ? tmpFechaConstancia.Year : o.fec_elaboracion.Year) &&
+                                                              o.fec_elaboracion.Month == (tmpFechaConstancia.Month > 0 ? tmpFechaConstancia.Month : o.fec_elaboracion.Month) &&
+                                                              o.fec_elaboracion.Day == (tmpFechaConstancia.Day > 0 ? tmpFechaConstancia.Day : o.fec_elaboracion.Day)).ToList()
+                            .Select(x => new
+                            {
+                                nroConstancia = x.num_constancia_preparado,
+                                nomPreparado = x.ALP_ORDEN_PREPARADO.ALP_RECETA.nom_preparado,
+                                fechaConstancia = x.fec_elaboracion.ToString("dd/MM/yyyy"),
+                                sucursal = x.ALP_ORDEN_PREPARADO.RRH_Sucursal.Nom_sucursal
+                            }).ToList();
+
+
+                return Json(JsonResponseFactory.SuccessResponse(constancia.ToList()));
+
+
+            }
+            catch (Exception ex)
+            {
+                return Json(JsonResponseFactory.ErrorResponse(ex.Message));
+            }
+
+        }
+
         public ActionResult Nuevo(string num_orden = null)
         {
 
@@ -48,6 +106,20 @@ namespace WebBS.Controllers.ALP
 
             var ordenPreparado = db.ALP_ORDEN_PREPARADO.Where( o => o.num_orden_preparado.Contains(num_orden));
             return View("~/Views/ALP/Constancia/Nuevo.cshtml", ordenPreparado.FirstOrDefault());
+        }
+
+        public ActionResult Modificar(string num_constancia = null)
+        {
+
+            var constancia = db.ALP_CONSTANCIA_PREPARADO.Where(o => o.num_constancia_preparado.Contains(num_constancia)).FirstOrDefault();
+
+            //ViewBag.tecnicoLaboratorista = constancia. .ALP_ORDEN_PREPARADO.RRH_Empleado.Nom_empleado + " " + constancia.ALP_ORDEN_PREPARADO.RRH_Empleado.Ap_paterno + " " + constancia.ALP_ORDEN_PREPARADO.RRH_Empleado.Ap_materno;
+            ViewBag.tecnicoFarmaceutico = constancia.ALP_ORDEN_PREPARADO.RRH_Empleado.Nom_empleado + " " + constancia.ALP_ORDEN_PREPARADO.RRH_Empleado.Ap_paterno + " " + constancia.ALP_ORDEN_PREPARADO.RRH_Empleado.Ap_materno;
+            ViewBag.fechaConstancia = constancia.fec_elaboracion.ToString("dd/MM/yyyy");
+
+
+            return View("~/Views/ALP/Constancia/Modificar.cshtml", constancia);
+
         }
 
         [HttpPost]
@@ -126,6 +198,57 @@ namespace WebBS.Controllers.ALP
             }
 
             return Json(JsonResponseFactory.SuccessResponse(nextNroConstancia));
+        }
+
+        [HttpPost]
+        public ActionResult Actualizar(string nroConstancia, string motivo, string[] insumos)
+        {
+
+            try
+            {
+
+                var manager = ((IObjectContextAdapter)db).ObjectContext.ObjectStateManager;
+
+                var constancia = db.ALP_CONSTANCIA_PREPARADO.Where(x => x.num_constancia_preparado == nroConstancia).FirstOrDefault();
+                constancia.motivo = motivo;
+
+                db.ALP_CONSTANCIA_PREPARADO.Attach(constancia);
+                manager.ChangeObjectState(constancia, EntityState.Modified);
+
+
+                var constanciaXInsumoDel = db.ALP_CONSTANCIA_PREPARADO_INSUMO.Where(x => x.num_constancia_preparado == nroConstancia).ToList();
+
+                foreach (ALP_CONSTANCIA_PREPARADO_INSUMO item in constanciaXInsumoDel)
+                {
+                    db.ALP_CONSTANCIA_PREPARADO_INSUMO.Remove(item);
+                    manager.ChangeObjectState(item, EntityState.Deleted);
+                }
+
+                for (int i = 0; i < insumos.Length; i++)
+                {
+                    string[] insumoSplit = insumos[i].Split('|');
+
+                    db.ALP_CONSTANCIA_PREPARADO_INSUMO.Add(new ALP_CONSTANCIA_PREPARADO_INSUMO()
+                    {
+                        num_constancia_preparado = nroConstancia,
+                        cod_insumo = insumoSplit[0],
+                        cant_insumo_orden = int.Parse(insumoSplit[1]),
+                        cant_insumo_constancia = int.Parse(insumoSplit[2]),
+                        cant_insumo_diferencia = int.Parse(insumoSplit[3])
+
+                    });
+                }
+
+                db.SaveChanges();
+
+            }
+            catch (Exception ex)
+            {
+                return Json(JsonResponseFactory.ErrorResponse(ex.Message));
+            }
+
+            return Json(JsonResponseFactory.SuccessResponse(nroConstancia));
+
         }
 
     }
---
+++ WebBS/Controllers/ALP/ControlMermaController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBS.Models;
+using WebBS.Models.ALP;
+
+namespace WebBS.Controllers.ALP
+{
+    public class ControlMermaController : Controller
+    {
+
+        private BDBoticasEntities db = new BDBoticasEntities();
+
+        //
+        // GET: /ControlMerma/
+        public ActionResult Lista()
+        {
+            List<SelectListItem> selectList = new List<SelectListItem>();
+
+            SelectListItem t = new SelectListItem();
+            t.Text = "Todos";
+            t.Value = "-1";
+            selectList.Add(t);
+
+            foreach (RRH_Sucursal c in db.RRH_Sucursal.ToList())
+            {
+                SelectListItem i = new SelectListItem();
+                i.Text = c.Nom_sucursal.ToString();
+                i.Value = c.Cod_sucursal.ToString();
+                selectList.Add(i);
+            }
+
+            ViewBag.Sucursales = selectList;
+
+            return View("~/Views/ALP/ControlMerma/Lista.cshtml");
+        }
+
+        public ActionResult Nuevo(string num_constancia = null)
+        {
+            var constancia = db.ALP_CONSTANCIA_PREPARADO.Where(o => o.num_constancia_preparado.Contains(num_constancia)).FirstOrDefault();
+
+            //ViewBag.tecnicoLaboratorista = constancia. .ALP_ORDEN_PREPARADO.RRH_Empleado.Nom_empleado + " " + constancia.ALP_ORDEN_PREPARADO.RRH_Empleado.Ap_paterno + " " + constancia.ALP_ORDEN_PREPARADO.RRH_Empleado.Ap_materno;
+            ViewBag.tecnicoFarmaceutico = constancia.ALP_ORDEN_PREPARADO.RRH_Empleado.Nom_empleado + " " + constancia.ALP_ORDEN_PREPARADO.RRH_Empleado.Ap_paterno + " " + constancia.ALP_ORDEN_PREPARADO.RRH_Empleado.Ap_materno;
+            ViewBag.fechaConstancia = constancia.fec_elaboracion.ToString("dd/MM/yyyy");
+
+
+            return View("~/Views/ALP/ControlMerma/Nuevo.cshtml", constancia);
+        }
+
+        [HttpPost]
+        public ActionResult Guardar(string nroConstancia, string[] insumos)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                return Json(JsonResponseFactory.ErrorResponse(ex.Message));
+            }
+
+            return Json(JsonResponseFactory.SuccessResponse());
+        }
+	}
+}
