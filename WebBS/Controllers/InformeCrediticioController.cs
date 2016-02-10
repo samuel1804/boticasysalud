40b2512c68f5f2c498718d51db0fabf2f2532186
diego <diegolopezcdm@gmail.com>
2016-02-09 20:54:26 -0500

    fixed bugs

--- WebBS/Controllers/InformeCrediticioController.cs
+++ WebBS/Controllers/InformeCrediticioController.cs
@@ -215,7 +215,7 @@ namespace WebBS.Controllers
 
             informe.GCC_SOLICITUD_CREDITO.Rango_credito = "<<" + plan.Rango_inicio + " a " + plan.Rango_fin + ">>";
             informe.GCC_SOLICITUD_CREDITO.Estado_informe = politica.Estado_resultante;
-            informe.GCC_SOLICITUD_CREDITO.Cantidad_credito = Convert.ToInt32((plan.Rango_inicio + plan.Rango_fin) / 2);
+            informe.GCC_SOLICITUD_CREDITO.Cantidad_credito = Convert.ToInt32(plan.Rango_inicio);//Convert.ToInt32((plan.Rango_inicio + plan.Rango_fin) / 2);
             informe.GCC_SOLICITUD_CREDITO.Codigo_politica = politica.Cod_politica_credito; 
             return RedirectToAction("Create", informe.GCC_SOLICITUD_CREDITO);
         }
@@ -366,6 +366,60 @@ namespace WebBS.Controllers
         public ActionResult GenerarContrato(int id)
         {
             return View(db.GCC_INFORME_CREDITICIO.Where(b => b.Cod_informe_crediticio == id).Single());
+        }
+         [HttpPost]
+        public ActionResult GenerarContrato(GCC_INFORME_CREDITICIO gccInf)
+        {
+            try
+            {
+
+                GCC_EMPLEADO_SOL_CREDITO gccEstSol = new GCC_EMPLEADO_SOL_CREDITO();
+                gccEstSol.Cod_solicitud_credito = gccInf.Cod_solicitud_credito;
+                gccEstSol.Cod_empleado = 1;
+                gccEstSol.Fec_registro = DateTime.Now;
+                gccEstSol.Estado = "G";
+                gccEstSol.Cod_usu_regi = 1;
+                gccEstSol.Fec_usu_regi = DateTime.Now;
+                db.GCC_EMPLEADO_SOL_CREDITO.Add(gccEstSol);
+                db.SaveChanges();
+
+                GCC_EMPLEADO_INF_CREDITICIO gccEstInf = new GCC_EMPLEADO_INF_CREDITICIO();
+                gccEstInf.Cod_informe_crediticio = gccInf.Cod_informe_crediticio;
+                gccEstInf.Cod_empleado = 1;
+                gccEstInf.Fec_registro = DateTime.Now;
+                gccEstInf.Estado = "G";
+                gccEstInf.Cod_usu_regi = 1;
+                gccEstInf.Fec_usu_regi = DateTime.Now;
+                db.GCC_EMPLEADO_INF_CREDITICIO.Add(gccEstInf);
+                db.SaveChanges();
+
+                GCC_CONTRATO_CREDITO gccCont = new GCC_CONTRATO_CREDITO();
+                gccCont.Cod_solicitud_credito = gccInf.Cod_solicitud_credito;
+                gccCont.Fec_inicio = DateTime.Now;
+                gccCont.Fec_renovacion = DateTime.Today.Date.AddYears(1);
+                gccCont.Cod_usu_regi = 1;
+                gccCont.Fec_usu_regi = DateTime.Now;
+                db.GCC_CONTRATO_CREDITO.Add(gccCont);
+                db.SaveChanges();
+
+                GCC_CUENTA_CLIENTE gccCta = new GCC_CUENTA_CLIENTE();
+                gccCta.Cod_contrato_credito = gccCont.Cod_contrato_credito;
+                gccCta.Num_cuenta = "00001";
+                gccCta.Linea_credito = gccInf.Monto_linea_credito_aprob;
+                gccCta.Linea_disponible = gccInf.Monto_linea_credito_aprob;
+                gccCta.Estado_cuenta = "A";
+                gccCta.Cod_usu_regi = 1;
+                gccCta.Fec_usu_regi = DateTime.Now;
+                db.GCC_CUENTA_CLIENTE.Add(gccCta);
+                db.SaveChanges();
+
+                return RedirectToAction("GenerarContratoIndex");
+            }
+            catch (System.ArgumentOutOfRangeException ex)
+            {
+                System.Console.WriteLine(ex.Message);
+                return View();
+            }
         }
 
         // GET: InformeCrediticio/Delete/5
